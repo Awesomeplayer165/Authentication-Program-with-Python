2c60b8d60600140e855e0ff1f278265a4ce31373
Jacob Trentini <70717139+Awesomeplayer165@users.noreply.github.com>
2021-10-21 20:02:59 +0000

    Finished the display function and changed the 100 chars functions to be inversed

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
-class Evaluater:
+# main.py
 
-	password = ""
+import Evaluater
 
-	def __init__(self, password):
-		self.password = password
+## GitHub Repo: https://github.com/Awesomeplayer165/Authentication-Program-with-Python
 
-	@property
-	def hasAtLeast12Characters(self) -> bool:
-		return len(self.password) >= 12
-
-	@property
-	def hasNumbers(self) -> bool:
-		for char in self.password:
-			try:    int(char); return True
-			except: pass
-		return False
-
-	@property
-	def hasSpecialCharacters(self) -> bool:
-		for char in self.password:
-			if char in ["!", "@", "#", "%", "^", "&", "~", "<", ">", "?", "/", "[", "]", ".", "|", "}", "{"]:
-				return True
-		
-		return False
-
-	def display(self):
-		print("Your password", ("has" if self.hasAtLeast12Characters else "does not have"), end=" at least 12 characters\n")
-		print("Your password", ("has" if self.hasNumbers             else "does not have"), end=" numbers\n")
-		print("Your password", ("has" if self.hasSpecialCharacters   else "does not have"), end=" special characters\n")
-
-eval = Evaluater(input("Enter a password: "))
+eval = Evaluater.Evaluater(input("Enter a password: "), input("Enter your name: "), input("Enter your username: "))
 eval.display()
